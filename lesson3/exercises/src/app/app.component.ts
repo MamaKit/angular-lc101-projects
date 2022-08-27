d63d3a47d346426c253e6f91379c002b1420de4e
MamaKit <mama.kit.pyles@gmail.com>
2022-08-27 12:12:13 -0500

    Functional Control Buttons completed.

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,35 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+
+  handleLand() {
+    let result = window.confirm("Are you sure you want to land?");
+    if (result) {
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = "Shuttle has landed";
+    }
+  }
+
+  handleAbort() {
+    let result = window.confirm("Are you sure you want to abort the mission?");
+    if (result) {
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = "Mission aborted.";
+    }
+  }
+
 }
