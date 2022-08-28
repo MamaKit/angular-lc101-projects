ae801bfd6e2e98881c49f01ecad0d164b3fe317c
MamaKit <mama.kit.pyles@gmail.com>
2022-08-27 22:33:18 -0500

    Completed all but last required item. What IS 'too low or too high'?!

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,50 +12,60 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
 
-  handleTakeOff() {
+  handleTakeOff(rocketImage) {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
     if (result) {
       this.color = 'blue';
       this.height = 10000;
       this.width = 0;
       this.message = 'Shuttle in flight.';
+      rocketImage.style.left = 0;
+      rocketImage.style.bottom = 0;
     }
+    this.takeOffEnabled = !this.takeOffEnabled;
   }
 
   handleLand(rocketImage) {
-    let result = window.confirm("Are you sure you want to land?");
-    if (result) {
-      this.color = 'green';
-      this.height = 0;
-      this.width = 0;
-      this.message = "Shuttle has landed.";
-      rocketImage.style.bottom = 0;
-      rocketImage.style.left = 0;
+    if(!this.takeOffEnabled) {
+      let result = window.confirm("Are you sure you want to land?");
+      if (result) {
+        this.color = 'green';
+        this.height = 0;
+        this.width = 0;
+        this.message = "Shuttle has landed.";
+        rocketImage.style.bottom = 0;
+        rocketImage.style.left = 0;
+      }
     }
+    this.takeOffEnabled = !this.takeOffEnabled;
   }
 
   handleAbort(rocketImage) {
-    let result = window.confirm("Are you sure you want to abort the mission?");
-    if (result) {
-      this.color = 'green';
-      this.height = 0;
-      this.width = 0;
-      this.message = "Mission aborted.";
-      rocketImage.style.bottom = 0;
-      rocketImage.style.left = 0;
+    if (!this.takeOffEnabled) {
+      let result = window.confirm("Are you sure you want to abort the mission?");
+      if (result) {
+        this.color = 'green';
+        this.height = 0;
+        this.width = 0;
+        this.message = "Mission aborted.";
+        rocketImage.style.bottom = 0;
+        rocketImage.style.left = 0;
+      }
     }
+    this.takeOffEnabled = !this.takeOffEnabled;
   }
 
   moveRocket(rocketImage,direction: string) {
-    if(this.message === 'Shuttle in flight.') {
+    if(this.message !== "Shuttle lost in space" && this.message !== "Shuttle crashed.") {
       if(direction === 'up') {
         if(parseInt(rocketImage.style.bottom) < 330) {
           let movementU = parseInt(rocketImage.style.bottom) + 10 + 'px';
           rocketImage.style.bottom = movementU;
           this.height = this.height + 10000;
         } else {
-          this.message = "Rocket lost in space.";
+          this.message = "Shuttle lost in space.";
           this.color = 'black';
           rocketImage.style.visibility = 'hidden';
         };
@@ -68,6 +78,7 @@ export class AppComponent {
           this.color = 'red';
           rocketImage.style.bottom = 0;
           this.message = 'Shuttle crashed.';
+          this.takeOffEnabled = !this.takeOffEnabled;
         };
       } else if (direction === 'right') {
         if(parseInt(rocketImage.style.left) < 450) {
@@ -75,7 +86,7 @@ export class AppComponent {
           rocketImage.style.left = movementR;
           this.width = this.width + 10000;
         } else {
-          alert("Rocket leaving defined mission area.");
+          alert("WARNING: Rocket leaving defined mission area.");
         };
       } else {
         if(parseInt(rocketImage.style.left) > 0) {
@@ -83,13 +94,8 @@ export class AppComponent {
           rocketImage.style.left = movementL;
           this.width = this.width - 10000;
         } else {
-          alert("Rocket leaving defined mission area.");
+          alert("WARNING: Rocket leaving defined mission area.");
         };
-      }
-    } else {
-      let result = window.confirm("Do you wish to launch the shuttle?");
-      if (result) {
-        this.handleTakeOff();
       }
     }
   }
